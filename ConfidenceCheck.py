81200f2ca6804d0bc7d452ccb3698d68663a74da
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-06-17 07:55:57 -0700

    Update ConfidenceCheck.py

--- ConfidenceCheck.py
+++ ConfidenceCheck.py
@@ -3,7 +3,10 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 
-
+""" Note: Returns the confidence score of the objected detected
+          Omits first object detected since that's likely the user 
+          since it's a first person shooter the game play view has the user in full view and is usually detected first.
+    TODO: Not just choose the second most confident score, but ensure only the enemy is identified not the user """
 def run(tensor):
     try:
         Object2 = tensor[1] #Get Second Object Detected
