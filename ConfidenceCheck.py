4401bbbf9562ce093bd9abb22d0fbcf75193074f
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-06-21 17:37:38 -0700

    Update ConfidenceCheck.py

--- ConfidenceCheck.py
+++ ConfidenceCheck.py
@@ -5,15 +5,33 @@ import numpy as np
 
 """ Note: Returns the confidence score of the objected detected
           Omits first object detected since that's likely the user 
-          since it's a first person shooter the game play view has the user in full view and is usually detected first.
-    TODO: Not just choose the second most confident score, but ensure only the enemy is identified not the user """
+          since it's a first person shooter the game play view has the user in full view and is usually detected first. """
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+
 def run(tensor):
-    try:
-        Object2 = tensor[1] #Get Second Object Detected
-        n = Object2.tolist() #Convert tensor tolist
-        x = torch.Tensor([n]) #Create new 2D tensor with one entry, n
-        return x
-    except: #Multiple objects have not been detected
+    try: #Multiple objects have been detected
+        InitialConfidence = tensor[0][5] #Confidence of initial objected detected
+        
+        PrepReturnTensor = tensor[0]
+
+        NumberofTensors = 0
+        for t in tensor: 
+            #Iterate through each tensor. If confidence is higher than intial confedence then update return tensor
+            CurrentConfidence = t[5]
+    
+            if CurrentConfidence > InitialConfidence:
+                PrepReturnTensor = t
+                
+            NumberofTensors+=1
+        n = PrepReturnTensor.tolist() #Convert PrepReturnTensor tolist
+        ReturnTensor = torch.Tensor([n]) #Create new 2D tensor with one entry, n
+           
+        return ReturnTensor
+
+    except: #One object detected
         print("Exception")
         return tensor
 
