f34a005764881bbe8d0b68816f0ff31a06bef851
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-06-24 16:56:14 -0700

    Update ConfidenceCheck.py

--- ConfidenceCheck.py
+++ ConfidenceCheck.py
@@ -2,27 +2,24 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
+from time import sleep
 
-def run(tensor):
+def run(tensor): #Only Display Object With Highest Confidence Score
+    """TODO: Return only object with the second highest score """
     try:
-        InitialConfidence = tensor[0][5]
-        
+        InitialConfidence = tensor[0][5].data.tolist()
         PrepReturnTensor = tensor[0]
-        
-        print("InitialtConfidence: ", InitialConfidence)
-        
+
         NumberofTensors = 0
         for t in tensor:
-            CurrentConfidence = t[5]
-            print("CurrentConfidence: ", CurrentConfidence)
+            CurrentConfidence = t[5].data.tolist()
+            #print("CurrentConfidence: ", CurrentConfidence)
             if CurrentConfidence > InitialConfidence:
-                print("CurrentConfidence > InitialConfidence: ", CurrentConfidence > InitialConfidence)
                 PrepReturnTensor = t
                 
             NumberofTensors+=1
         n = PrepReturnTensor.tolist() #Convert PrepReturnTensor tolist
-        ReturnTensor = torch.Tensor([n]) #Create new 2D tensor with one entry, n
-           
+        ReturnTensor = torch.Tensor([n]).cuda() #Create new 2D tensor with one entry, n
         return ReturnTensor
 
     except: #Multiple objects have not been detected
@@ -32,9 +29,34 @@ def run(tensor):
 
 def score(tensor):
     try:
-        confidence = tensor[5]
+        confidence = tensor[5].data.tolist()
         return confidence
-
+        
     except: #Multiple objects have not been detected
         print("Exception: ConfidenceCheck.score() error")
         return 0    
+    
+def SecondHighest(tensor): 
+    NumberObjectsdDetected = len(tensor)
+    if NumberObjectsdDetected > 1: #Multiple Objects
+        try:
+            InitialConfidence = tensor[0][5].data.tolist()
+            PrepReturnTensor = tensor[0]
+
+            NumberofTensors = 0
+            for t in tensor:
+                CurrentConfidence = t[5].data.tolist()
+                #print("CurrentConfidence: ", CurrentConfidence)
+                if CurrentConfidence > InitialConfidence:
+                    PrepReturnTensor = t
+                
+                NumberofTensors+=1
+            n = PrepReturnTensor.tolist() #Convert PrepReturnTensor tolist
+            ReturnTensor = torch.Tensor([n]).cuda() #Create new 2D tensor with one entry, n
+            return ReturnTensor
+
+        except: #Multiple objects have not been detected
+            print("Exception: ConfidenceCheck.run() error")
+            return tensor
+  
+    return
