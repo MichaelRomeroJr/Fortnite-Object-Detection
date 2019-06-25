fbe10711ae16d6bb1a3744f5431420966eac39d7
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-06-25 02:45:03 -0700

    Update ConfidenceCheck.py

--- ConfidenceCheck.py
+++ ConfidenceCheck.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
 from time import sleep
+import time
 
 def run(tensor): #Only Display Object With Highest Confidence Score
     """TODO: Return only object with the second highest score """
@@ -23,9 +24,8 @@ def run(tensor): #Only Display Object With Highest Confidence Score
         return ReturnTensor
 
     except: #Multiple objects have not been detected
-        print("Exception: ConfidenceCheck.run() error")
+        #print("Exception: ConfidenceCheck.run() error")
         return tensor
-
 
 def score(tensor):
     try:
@@ -37,26 +37,26 @@ def score(tensor):
         return 0    
     
 def SecondHighest(tensor): 
-    NumberObjectsdDetected = len(tensor)
-    if NumberObjectsdDetected > 1: #Multiple Objects
-        try:
-            InitialConfidence = tensor[0][5].data.tolist()
-            PrepReturnTensor = tensor[0]
+    try:
+        NumberObjectsdDetected = len(tensor)
+        ListOfTensors = []
+        if NumberObjectsdDetected > 1: #Multiple Objects
+            for i in range(NumberObjectsdDetected):
+                ListOfTensors.append(tensor[i].tolist())
+        
+            ListOfTensorsSorted = sorted(ListOfTensors, key=lambda x: x[5], reverse=True)
 
-            NumberofTensors = 0
-            for t in tensor:
-                CurrentConfidence = t[5].data.tolist()
-                #print("CurrentConfidence: ", CurrentConfidence)
-                if CurrentConfidence > InitialConfidence:
-                    PrepReturnTensor = t
-                
-                NumberofTensors+=1
-            n = PrepReturnTensor.tolist() #Convert PrepReturnTensor tolist
-            ReturnTensor = torch.Tensor([n]).cuda() #Create new 2D tensor with one entry, n
-            return ReturnTensor
-
-        except: #Multiple objects have not been detected
-            print("Exception: ConfidenceCheck.run() error")
-            return tensor
-  
-    return
+            for i in ListOfTensorsSorted:
+                pass
+        
+            SecondHighestList = ListOfTensorsSorted[1]  
+            ReturnTensor = torch.Tensor([SecondHighestList]).cuda()
+        
+        else: #One Object Detected
+            ReturnTensor = tensor
+        
+        return ReturnTensor
+    except:
+        print("Exception")
+        return tensor
+    
