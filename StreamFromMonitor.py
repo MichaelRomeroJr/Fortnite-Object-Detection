711ae17aae9e0459ce2f654bddd80557c451a52d
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-06-28 11:42:25 -0700

    Update StreamFromMonitor.py

--- StreamFromMonitor.py
+++ StreamFromMonitor.py
@@ -6,7 +6,9 @@ from time import sleep
 from PIL import Image
 
 def run():
-    img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
+    img = ImageGrab.grab(bbox=(100,100,190,100)) #bbox specifies specific region (bbox= x,y,width,height)
+    print(type())
+    #img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
     img_np = np.array(img) 
     im = Image.fromarray(img_np)
     im.save("C:/Users/micha/OneDrive/Desktop/Screen.jpeg") 
@@ -17,6 +19,8 @@ def run():
     
 def Stream():
     img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
+    """Sample a smaller area of the monitor"""
+    #img = ImageGrab.grab(bbox=(175,133,1750,950)) #bbox specifies specific region (bbox= x,y,width,height) 
     img_np = np.array(img) 
     #cv2.waitKey(1)
     
