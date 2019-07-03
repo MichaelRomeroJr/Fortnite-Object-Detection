357851b992d98651924fcebe4ee07cecb59d3ae0
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-07-03 14:07:03 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,35 +7,10 @@ from pynput import keyboard
 
 import DetectWithCUDA
 
-Going = True
+Going = False
+#Going = True
 while Going:
+    s = time.time()
     DetectWithCUDA.Running()
-
-def on_press(key):
-    try: #alphanumeric key
-        keypressed = key.char
-        print("key: ", keypressed)
-    except AttributeError: #special key
-        keypressed = key
-        print("key: w", keypressed)
-
-    if keypressed == 'f': #print("You pressed F")
-        print("Run AutoDetection")
-        DetectWithCUDA.Running()
-
-def on_release(key):
-    print('{0} released'.format(key))
-    if key == keyboard.Key.esc: # Stop listener    
-        return False
-
-# Collect events until released
-with keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
-
-# ...or, in a non-blocking fashion:
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
+    f =time.time()
+    print(f - s)
