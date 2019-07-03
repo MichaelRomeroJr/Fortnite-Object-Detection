c28e8a923111c58f7d4a8d298ad85cf2001eead0
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-07-03 14:08:10 -0700

    Update StreamFromMonitor.py

--- StreamFromMonitor.py
+++ StreamFromMonitor.py
@@ -5,22 +5,12 @@ import cv2
 from time import sleep
 from PIL import Image
 
-def run():
-    img = ImageGrab.grab(bbox=(100,100,190,100)) #bbox specifies specific region (bbox= x,y,width,height)
-    print(type())
-    #img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
-    img_np = np.array(img) 
-    im = Image.fromarray(img_np)
-    im.save("C:/Users/micha/OneDrive/Desktop/Screen.jpeg") 
-    cv2.waitKey(1)
-    
-    return
-#cv2.destroyAllWindows()
-    
+
 def Stream():
-    img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
+    """Read entire monitor as a Pil.Image """
+    #img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
     """Sample a smaller area of the monitor"""
-    #img = ImageGrab.grab(bbox=(175,133,1750,950)) #bbox specifies specific region (bbox= x,y,width,height) 
+    img = ImageGrab.grab(bbox=(175,133,1750,950)) #bbox specifies specific region (bbox= x,y,width,height) 
     img_np = np.array(img) 
     #cv2.waitKey(1)
     
