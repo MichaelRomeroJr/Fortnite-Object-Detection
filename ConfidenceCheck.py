7eb4b11e1cd18800819081b122849ede247c1714
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2019-07-03 14:10:27 -0700

    Update ConfidenceCheck.py

--- ConfidenceCheck.py
+++ ConfidenceCheck.py
@@ -1,45 +1,98 @@
-# -*- coding: utf-8 -*-
-from __future__ import division
-import torch 
+import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import numpy as np
-import cv2 
-from util import *
-import argparse
-import os 
-import os.path as osp
-from darknet import Darknet
-import pickle as pkl
-import pandas as pd
-import random
-import time
 from time import sleep
+import time
 
-from PIL import Image, ImageTk
+from util import *
 
-import CreateOverlay
-import ConfidenceCheck
+def run(tensor): #Only Display Object With Highest Confidence Score
+    """TODO: Return only object with the second highest score """
+    try:
+        InitialConfidence = tensor[0][5].data.tolist()
+        PrepReturnTensor = tensor[0]
 
-import StreamFromMonitor
+        NumberofTensors = 0
+        for t in tensor:
+            CurrentConfidence = t[5].data.tolist()
+            #print("CurrentConfidence: ", CurrentConfidence)
+            if CurrentConfidence > InitialConfidence:
+                PrepReturnTensor = t
+                
+            NumberofTensors+=1
+        n = PrepReturnTensor.tolist() #Convert PrepReturnTensor tolist
+        ReturnTensor = torch.Tensor([n]).cuda() #Create new 2D tensor with one entry, n
+        return ReturnTensor
 
-def Rectangle(tensor):
-    RectanglePath = "C:/Users/micha/OneDrive/Desktop/Red.jpg"
+    except: #Multiple objects have not been detected
+        #print("Exception: ConfidenceCheck.run() error")
+        return tensor
+
+def score(tensor):
+    try:
+        confidence = tensor[5].data.tolist()
+        return confidence
+        
+    except: #Multiple objects have not been detected
+        print("Exception: ConfidenceCheck.score() error")
+        return 0    
+    
+def SecondHighest(tensor): 
+    try:
+        NumberObjectsdDetected = len(tensor)
+        ListOfTensors = []
+        if NumberObjectsdDetected > 1: #Multiple Objects
+            for i in range(NumberObjectsdDetected):
+                ListOfTensors.append(tensor[i].tolist())
+        
+            ListOfTensorsSorted = sorted(ListOfTensors, key=lambda x: x[5], reverse=True)
+
+            for i in ListOfTensorsSorted:
+                pass
+        
+            SecondHighestList = ListOfTensorsSorted[1]  
+            ReturnTensor = torch.Tensor([SecondHighestList]).cuda()
+        
+        else: #One Object Detected
+            ReturnTensor = tensor
+        
+        return ReturnTensor
+    except:
+        #print("Exception")
+        return tensor
+"""    
+def Position Check(tensor):
+    #The Loswest Coordinate is PROBABLY yourself so don't shoot the lowest x,y coordinate
     c1 = tuple(tensor[1:3].int()) #Top Left Coordinates
     c2 = tuple(tensor[3:5].int()) #Bottom Right Coordinates
     
-    x1 = int(c1[0].item()) #+175
-    y1 = int(c1[1].item()) #+133
-    x2 = int(c2[0].item()) #+175
-    y2 = int(c2[1].item()) #+133
+    x1 = int(c1[0].item())
+    y1 = int(c1[1].item())
+    x2 = int(c2[0].item())
+    y2 = int(c2[1].item())
      
-    width = x2 -  x1
-    height = y2 - y1 
-
-    return width, height, x1, y1, RectanglePath
-
-def run(tensor):
-    width, height, x, y,RectanglePath = Rectangle(tensor)
-    CreateOverlay.run(RectanglePath, width, height, x, y)
     
-    return
+    try:
+        NumberObjectsdDetected = len(tensor)
+        ListOfTensors = []
+        if NumberObjectsdDetected > 1: #Multiple Objects
+            for i in range(NumberObjectsdDetected):
+                ListOfTensors.append(tensor[i].tolist())
+        
+            ListOfTensorsSorted = sorted(ListOfTensors, key=lambda x: x[5], reverse=True)
+
+            for i in ListOfTensorsSorted:
+                pass
+        
+            SecondHighestList = ListOfTensorsSorted[1]  
+            ReturnTensor = torch.Tensor([SecondHighestList]).cuda()
+        
+        else: #One Object Detected
+            ReturnTensor = tensor
+        
+        return ReturnTensor
+    except:
+        #print("Exception")
+        return tensor
+        """
