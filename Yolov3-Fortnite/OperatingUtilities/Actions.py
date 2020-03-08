05476408b9e0aa61ce283ab7ce921850675c92fc
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2020-03-07 20:38:31 -0800

    Update Actions.py

--- Yolov3-Fortnite/OperatingUtilities/Actions.py
+++ Yolov3-Fortnite/OperatingUtilities/Actions.py
@@ -2,16 +2,18 @@
 import pyautogui
 
 def Fire(x, y, width, height):
+    """Fire when cursor is in displayed rectangle """
     coordinates = pyautogui.position()
     x1, y1 = x, y
     x2, y2 = x+width, y+height
          
+    #Note: Fortnite cursor stays fixed in center of screen (the crosshairs)
     XCenter = 960.0
     YCenter = 540.0
     
     if (x1<XCenter<x2) & (y1<YCenter<y2): #Current mouse position is in rectangle
         print("Press Down")
-        pyautogui.mouseDown()   # does the same thing as a left-button mouse click
+        pyautogui.mouseDown() # does the same thing as a left-button mouse click
         sleep(1)
         pyautogui.mouseUp()
         print("Press Released")
