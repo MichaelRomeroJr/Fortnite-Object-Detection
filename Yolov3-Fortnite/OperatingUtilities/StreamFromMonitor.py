1d599ecfb4f6aece34e99c8619ec6a0bef66f84e
MichaelRomeroJr <MichaelRomeroJr1@Gmail.com>
2020-03-07 20:28:11 -0800

    Update StreamFromMonitor.py

--- Yolov3-Fortnite/OperatingUtilities/StreamFromMonitor.py
+++ Yolov3-Fortnite/OperatingUtilities/StreamFromMonitor.py
@@ -3,11 +3,11 @@ from PIL import ImageGrab
 import numpy as np
 
 def Stream():
-    """Read entire monitor as a Pil.Image """
-    #img = ImageGrab.grab(bbox=(10,10,1900,1000)) #bbox specifies specific region (bbox= x,y,width,height)
-    """Sample a smaller area of the monitor as Pil.Image"""
-    #img = ImageGrab.grab(bbox=(175,133,1750,950)) #bbox specifies specific region (bbox= x,y,width,height) 
-    img = ImageGrab.grab(bbox=(0,0,1920,1080))
+    """Read entire monitor as a Pillow Image
+        then convert to Numpy Array
+        bbox specifies specific region (bbox= x,y,width,height)"""
+    #img = ImageGrab.grab(bbox=(175,133,1750,950)) #Sample a smaller area of the monitor as Pil.Image"""
+    img = ImageGrab.grab(bbox=(0,0,1920,1080) #Grab entire monitor as Pil.image
     img_np = np.array(img) 
     
     return img_np
